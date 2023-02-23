8c1cf9047d8d2e9b0dd65b5acf635e1171937a37
kolev92 <125755028+kolev92@users.noreply.github.com>
2023-02-23 18:37:22 +0200

    Add files via upload

---
+++ LCD_Hello_/LCD_Hello_.ino
@@ -0,0 +1,18 @@
+#include <LiquidCrystal.h>
+//LCD pin to Arduino
+const int pin_RS = 8; 
+const int pin_EN = 9; 
+const int pin_d4 = 4; 
+const int pin_d5 = 5; 
+const int pin_d6 = 6; 
+const int pin_d7 = 7; 
+const int pin_BL = 10; 
+LiquidCrystal lcd( pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
+void setup() {
+ lcd.begin(16, 2);
+ lcd.setCursor(0,0);
+ lcd.print("HELLO WORLD!");
+}
+void loop() {
+ 
+} 
